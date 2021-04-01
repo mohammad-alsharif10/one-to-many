131c1332db1263021782c2f93c26953ef7ac1ff3
mohammad alshsrif <mohammad-alsharif10@outlook.com>
2021-04-01 19:37:39 +0200

    one to many

--- src/main/java/com/example/demo/controller/DoctorController.java
+++ src/main/java/com/example/demo/controller/DoctorController.java
@@ -1,10 +1,14 @@
 package com.example.demo.controller;
 
 import com.example.demo.database.DoctorRepository;
+import com.example.demo.database.PatientRepository;
 import com.example.demo.model.Doctor;
+import com.example.demo.model.Patient;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import javax.print.Doc;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -13,15 +17,27 @@ import java.util.Optional;
 public class DoctorController {
 
     private final DoctorRepository doctorRepository;
+    private final PatientRepository patientRepository;
 
     @RequestMapping(value = "/save", method = RequestMethod.POST)
     public Doctor saveDoctor(@RequestBody Doctor doctor) {
-        return doctorRepository.save(doctor);
+        List<Patient>patientList=doctor.getPatientList();
+        doctor.setPatientList(null);
+        Doctor doctor1= doctorRepository.save(doctor);
+        patientList.forEach(patient -> {
+            patient.setDoctor(doctor1);
+            patientRepository.save(patient);
+        });
+        return doctorRepository.findById(doctor1.getId()).get();
     }
 
     @RequestMapping(value = "/find", method = RequestMethod.GET)
-    public Optional<Doctor> findDoctor(@RequestParam(name = "id") Integer id ) {
-        return doctorRepository.findById(id);
+    public Doctor findDoctor(@RequestParam(name = "id") Integer id ) {
+       Doctor doctor= doctorRepository.findById(id).get();
+       doctor.getPatientList().forEach(patient -> {
+           patient.setDoctor(null);
+       });
+       return doctor;
     }
 
 
